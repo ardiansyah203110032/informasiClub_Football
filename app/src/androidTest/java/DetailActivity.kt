0c2f407c4c3a181499b10881625646aca6f04a45
ardiansyah203110032 <102913641+ardiansyah203110032@users.noreply.github.com>
2022-12-25 22:23:03 +0700

    Add files via upload

---
+++ app/src/androidTest/java/DetailActivity.kt
@@ -0,0 +1,69 @@
+package com.ardiansyahmnur.informasiclub_football
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class DetailActivity : AppCompatActivity() {
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_STADIUM = "extra_stadium"
+        const val EXTRA_DETAIL = "extra_detail"
+        const val EXTRA_PHOTO = "extra_photo"
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val nameText: TextView = findViewById(R.id.textName)
+        val stadiumText: TextView = findViewById(R.id.textStadium)
+        val detailText : TextView = findViewById(R.id.textDetail)
+        val imgClub:ImageView = findViewById(R.id.imgClub)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val stadium = intent.getStringExtra(EXTRA_STADIUM)
+        val detail = intent.getStringExtra(EXTRA_DETAIL)
+        val img = intent.getIntExtra(EXTRA_PHOTO,0)
+
+
+
+        nameText.text = name
+        stadiumText.text = stadium
+        detailText.text = detail
+        Glide.with(this)
+            .load(img)
+            .apply(RequestOptions())
+            .into(imgClub)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.share_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, intent.getStringExtra(EXTRA_NAME))
+                    type = "text/plain"
+                }
+                startActivity(sendIntent)
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/HomeActivity.kt
@@ -0,0 +1,67 @@
+package com.ardiansyahmnur.informasiclub_football
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var rvClub: RecyclerView
+    private var list: ArrayList<Club> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        rvClub = findViewById(R.id.rv_clubs)
+        rvClub.setHasFixedSize(true)
+
+        list.addAll(ClubsData.listData)
+        showRecyclerList()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.about_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.about_page -> {
+                val moveIntent = Intent(this@HomeActivity, AboutActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+
+    private fun showSelectedHero(club: Club) {
+        val moveIntent = Intent(this@HomeActivity, DetailActivity::class.java)
+        moveIntent.putExtra(DetailActivity.EXTRA_NAME, club.name)
+        moveIntent.putExtra(DetailActivity.EXTRA_STADIUM, club.stadium)
+        moveIntent.putExtra(DetailActivity.EXTRA_DETAIL, club.detail)
+        moveIntent.putExtra(DetailActivity.EXTRA_PHOTO, club.photo)
+        startActivity(moveIntent)
+    }
+
+    private fun showRecyclerList() {
+        rvClub.layoutManager = LinearLayoutManager(this)
+        val listClubAdapter = ListClubAdapter(list)
+        rvClub.adapter = listClubAdapter
+
+        listClubAdapter.setOnItemClickCallback(object : ListClubAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Club) {
+                showSelectedHero(data)
+            }
+        })
+    }
+}
