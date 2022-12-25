35cdd2331e19b5fefbf7eb21be1c46431bff9c6e
ardiansyah203110032 <102913641+ardiansyah203110032@users.noreply.github.com>
2022-12-25 22:44:14 +0700

    Update ApiEndpoint.kt

--- app/src/androidTest/java/networking/ApiEndpoint.kt
+++ app/src/androidTest/java/networking/ApiEndpoint.kt
@@ -4,3 +4,7 @@ package com.ardiansyahmnur.informasiclub_football
 object ApiEndpoint {
     var BASEURL = "https://football.com/search?q={query}"
 }
+//pada kode program diatas memiliki sebuah fungsi untuk bertukar data dan sebuah antarmuka yang digunakan untuk menghubungkan antara satu aplikasi dengan aplikasi yang lain. 
+//Peran dari API adalah untuk sebagai perantara yang menghubungkan aplikasi berbeda, baik dari platform yang sama maupun lintas platform.
+//antarmuka yang digunakan oleh dua sistem komputer untuk bertukar informasi secara aman melalui internet.
+//Sebagian besar aplikasi ini berkomunikasi dengan aplikasi internal dan pihak ketiga lainnya untuk melakukan berbagai tugas.
