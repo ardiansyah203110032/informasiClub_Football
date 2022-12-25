4b83440cdc0e3a891382377ea1fe6d856a44fd32
ardiansyah203110032 <102913641+ardiansyah203110032@users.noreply.github.com>
2022-12-25 22:42:17 +0700

    Update MainActivity.kt

--- app/src/androidTest/java/MainActivity.kt
+++ app/src/androidTest/java/MainActivity.kt
@@ -18,3 +18,9 @@ class MainActivity : AppCompatActivity() {
         }, 3000)
     }
 }
+//pada kode program diatas merupakan salah satu komponen yang ada di Android Studio yang berfungsi untuk menampilkan user interface (UI) dari aplikasi yang akan dibuat, 
+//biasanya diletakkan pada “setcontentview”. Bukan hanya menampilkan UI, activity juga digunakan untuk melakukan berbagai kegiatan yang diperlukan di dalam aplikasi
+//dirancang untuk memfasilitasi paradigma ini. Jika satu aplikasi memanggil aplikasi lainnya, 
+//aplikasi pemanggil akan memanggil aktivitas di aplikasi lain, bukan aplikasi sebagai keseluruhan atomik.memiliki sebuah fungsi
+//Intent pada android merupakan sebuah class yang digunakan untuk berpindah activity atau halaman. 
+//Sedangkan activity sendiri merupakan suatu halaman yang dapat dilihat pengguna untuk berinteraksi dengan aplikasi.
