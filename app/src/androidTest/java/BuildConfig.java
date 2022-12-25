1a2dc494aa6479c3521959fb5d205c16f2cad2c5
ardiansyah203110032 <102913641+ardiansyah203110032@users.noreply.github.com>
2022-12-25 22:27:21 +0700

    Update BuildConfig.java

--- app/src/androidTest/java/BuildConfig.java
+++ app/src/androidTest/java/BuildConfig.java
@@ -1,9 +1,9 @@
 public class BuildConfig {
     public final class BuildConfig {
         public static final boolean DEBUG = Boolean.parseBoolean("true");
-        public static final String APPLICATION_ID = "com.ardidwibowo.clubepl";
+        public static final String APPLICATION_ID = "com.ardiansyahmnur.informasiclub_football";
         public static final String BUILD_TYPE = "debug";
         public static final int VERSION_CODE = 1;
         public static final String VERSION_NAME = "1.0";
     }
-//
+//memberikan akses type data.
