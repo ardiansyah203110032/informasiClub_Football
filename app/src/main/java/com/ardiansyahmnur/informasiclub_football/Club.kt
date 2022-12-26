3b643c57c6d9cef6bd05eacb988bbeb50cd6d1db
ardiansyah203110032 <102913641+ardiansyah203110032@users.noreply.github.com>
2022-12-26 18:38:27 +0700

    Update Club.kt

--- app/src/main/java/com/ardiansyahmnur/informasiclub_football/Club.kt
+++ app/src/main/java/com/ardiansyahmnur/informasiclub_football/Club.kt
@@ -6,3 +6,4 @@ data class Club(
     var detail : String = "",
     var photo: Int = 0
 )
+//memberikan akses variabel type data yang membuat nilai untuk deklarasi data dengan string dan detail pada image.
